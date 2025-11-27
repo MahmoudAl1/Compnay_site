e4a9c9223293351c23ec347d0d271eaa21facaca
MahmoudAl1 <mahmo6866u@gmail.com>
2025-11-27 16:17:43 +0200

    Update product images from PNG to JPG format

--- components/Products.tsx
+++ components/Products.tsx
@@ -12,7 +12,7 @@ const PRODUCTS_DATA: Product[] = [
     price: "0",
     capacity: "جميع السعات (35Ah - 200Ah)",
     warranty: "متاح",
-    image: "/images/fullstark.png",
+    image: "/images/fullstark.jpg",
     type: "local"
   },
   {
@@ -32,7 +32,7 @@ const PRODUCTS_DATA: Product[] = [
     price: "0",
     capacity: "جميع السعات (70Ah - 150Ah)",
     warranty: "متاح",
-    image: "/images/acdmax.png",
+    image: "/images/acdmax.jpg",
     type: "local"
   },
   {
@@ -94,7 +94,7 @@ const PRODUCTS_DATA: Product[] = [
     price: "0",
     capacity: "جميع السعات متوفرة",
     warranty: "متاح",
-    image: "/images/bosch.png",
+    image: "/images/bosch.jpg",
     type: "imported"
   },
   {
